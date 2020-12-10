b045fc5e0c07fb171946664dfcd3796b29877e5f
spence <tangguo1280@163.com>
2020-12-10 18:51:45 +0800

    update

---
+++ LHY_learning/.ipynb_checkpoints/hw1-checkpoint.py
@@ -0,0 +1,9 @@
+import sys
+import pandas as pd
+import numpy as np
+# from google.colab import drive
+
+# !gdown --id '1wNKAxQ29G15kgpBy_asjTcZRRgmsCZRm' --output data.zip
+# !unzip data.zip
+# data = pd.read_csv('gdrive/My Drive/hw1-regression/train.csv', header = None, encoding = 'big5')
+data = pd.read_csv('')
