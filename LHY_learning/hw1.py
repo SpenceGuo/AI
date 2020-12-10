bbedac49497d15e734ab5337a704682672592edc
tguo <tangguo1280@qq.com>
2020-12-10 18:33:21 +0800

    Update hw1.py

--- LHY_learning/hw1.py
+++ LHY_learning/hw1.py
@@ -6,4 +6,12 @@ import numpy as np
 # !gdown --id '1wNKAxQ29G15kgpBy_asjTcZRRgmsCZRm' --output data.zip
 # !unzip data.zip
 # data = pd.read_csv('gdrive/My Drive/hw1-regression/train.csv', header = None, encoding = 'big5')
-data = pd.read_csv('')
+data = pd.read_csv('data/hw1_data/train.csv', encoding='big5')
+print(data)
+
+data = data.iloc[:, 3:]
+print(data)
+
+data[data == "NR"] = 0
+raw_data = data.to_numpy()
+print(data)
