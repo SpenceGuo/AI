28f6544ddec66fc47b0dffe40ebf5a369629c69d
tguo <tangguo1280@163.com>
2021-02-23 15:20:51 +0800

    Create tree.py

---
+++ machineLearning/py/tree.py
@@ -0,0 +1,37 @@
+from math import log
+from typing import List
+
+
+def createDataset():
+    dataSet = [
+        [1, 1, 'yes'],
+        [1, 1, 'yes'],
+        [1, 0, 'no'],
+        [0, 1, 'no'],
+        [0, 1, 'no']
+    ]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+def calShannonEnt(dataSet: List):
+    # 数据集中实例总数
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+if __name__ == '__main__':
+    myDat, labels = createDataset()
+    print(myDat)
+    print(labels)
+    print(calShannonEnt(myDat))
