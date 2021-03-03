ef191b75ad5c76087c5b1262c28aea594f6950fb
tguo <tangguo1280@163.com>
2021-03-03 14:26:42 +0800

    Update CNN.py

--- deepLearning/py/CNN.py
+++ deepLearning/py/CNN.py
@@ -1,4 +1,22 @@
 import keras
 
-print(keras.__version__)
+from keras.models import Sequential
+from keras.layers import Dense, Convolution2D, MaxPooling2D, Flatten, Activation
 
+model = Sequential()
+
+# 第一层 Convolution 和 Maxpooling
+model.add(Convolution2D(25, 3, 3, input_shape=(1, 28, 28)))
+model.add(MaxPooling2D((2, 2)))
+
+# 第二层 Convolution 和 Maxpooling
+model.add(Convolution2D(50, 3, 3))
+model.add(MaxPooling2D((2, 2)))
+
+model.add(Flatten())
+
+# Fully Connected Feedforward network
+model.add(Dense(output_dim=100))
+model.add(Activation('relu'))
+model.add(Dense(output_dim=10))
+model.add(Activation('softmax'))
