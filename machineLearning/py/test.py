85c0e9708c61e917680a8d3186410685590bc3e3
tguo <tangguo1280@163.com>
2021-03-05 19:07:18 +0800

    Update test.py

--- machineLearning/py/test.py
+++ machineLearning/py/test.py
@@ -1,9 +1,16 @@
-import numpy as np
-import torch
-import torch.nn as nn
+import random
 
-print(np.zeros((3, 3), dtype=int))
+test = [1,1,1,1,2,3,3,3,3,3]
 
-a = ''
-
-print(len(a))
+count1 = 0
+count2 = 0
+count3 = 0
+for i in range(10000):
+    if random.choice(test) == 1:
+        count1 += 1
+    elif random.choice(test) == 2:
+        count2 += 1
+    else:
+        count3 += 1
+print('1-{} \n2-{} \n3-{}'.format(round(count1/10000, 3), round(count2/10000, 2),
+                                  round(count3/10000, 2)))
